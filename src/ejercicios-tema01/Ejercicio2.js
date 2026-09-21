0ba295b8f75d540c21643f4e76fcf62c37212410
robertobornos <robertobornos@.com>
2026-09-21 13:00:52 +0200

    Subida de ejercicio 1 y 2

--- src/ejercicios-tema01/Ejercicio2.js
+++ src/ejercicios-tema01/Ejercicio2.js
@@ -1,12 +1,15 @@
 function alerta() {
 
+    //Cremos la variables hijos que segun lo que responda se queda guardado
     var hijos = prompt("cuantos hijos los tienes ")
 
 
+   //comprobamos que no haya dejado nada en blanco
     if (hijos === null || hijos.trim() === '') {
         alert('no uede estar vacio');
     } else {
-        alert('Tienes ' + hijos);
+        //Mandamos una alerta con los hijos que tiene
+         alert('Tienes ' + hijos);
 
     }
 
