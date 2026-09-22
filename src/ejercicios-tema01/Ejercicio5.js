14abf4daddb6eec5a2660c4067a278b0c062c7aa
robertobornos <robertobornos@.com>
2026-09-22 13:01:56 +0200

    ejercicio 4

--- src/ejercicios-tema01/Ejercicio5.js
+++ src/ejercicios-tema01/Ejercicio5.js
@@ -1,8 +1,8 @@
 function sumar(){
 
 
-   var n1 = prompt("dame el primer numero")
-   var n2 = prompt("dame el segundo numero")
+    var n1 = Number(prompt("dame el primer numero"))
+    var n2 = Number(prompt("dame el segundo numero"))
 
     var total = n1 + n2
 
---
+++ src/ejercicios-tema01/Ejercicio6.js
@@ -0,0 +1,14 @@
+function calcular(){
+
+    var km = Number(prompt("dame el numero en km"))
+
+    var ms = km /3.6
+
+
+    alert(ms)
+
+
+
+
+
+}
